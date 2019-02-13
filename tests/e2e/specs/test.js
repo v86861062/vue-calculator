746831ae64743367483590b593f1d2b009632b90
v86861062 <v86861062@gmail.com>
2019-02-13 14:43:01 +0800

    Refactor e2e test

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -1,45 +1,43 @@
 // https://docs.cypress.io/api/introduction/api.html
 
+beforeEach(() => {
+  cy.visit("/")
+})
+
 describe("Smoke Test", () => {
   it("Visits the app root url", () => {
-    cy.visit("/")
     cy.title().should("eq", "vue-calculator")
   })
 })
 
 describe("Basic function Test", () => {
   it("Use mouse to enter an expression", () => {
-    cy.visit("/")
-
     clickButton("1")
     clickButton("2")
     clickButton("+")
     clickButton("3")
-    cy.wait(500)
+    waitForResult()
     clickButton("=")
+
     cy.contains("12+3 = 15")
   })
 
   it("Use keyboard to enter an expression", () => {
-    cy.visit("/")
+    enterExpression("5^9")
+    waitForResult()
+    enterExpression("{enter}")
 
-    cy.get("input").type("5^9")
-
-    cy.wait(500)
-    cy.get("input").type("{enter}")
     cy.contains(`5^9 = ${Math.pow(5, 9)}`)
   })
 
   it("Should clear results history", () => {
-    cy.visit("/")
+    enterExpression("5+6")
+    waitForResult()
+    enterExpression("{enter}")
 
-    cy.get("input").type("5+6")
-    cy.wait(500)
-    cy.get("input").type("{enter}")
-
-    cy.get("input").type("7-8")
-    cy.wait(500)
-    cy.get("input").type("{enter}")
+    enterExpression("7-8")
+    waitForResult()
+    enterExpression("{enter}")
 
     clickButton("AC")
 
@@ -47,28 +45,33 @@ describe("Basic function Test", () => {
   })
 
   it("Should display Error", () => {
-    cy.visit("/")
-
-    cy.get("input").type("5+-*/")
-    cy.wait(500)
+    enterExpression("5+-*/")
+    waitForResult()
 
     cy.get(".ans").contains("= error")
   })
 })
 
 describe("Abnormal use cases", () => {
-  it("should not update results", () => {
-    cy.visit("/")
+  it("Should not update results", () => {
+    clickButton("=")
+    clickButton("=")
+    clickButton("=")
 
-    clickButton("=")
-    clickButton("=")
-    clickButton("=")
     cy.get(".results").should("be.empty")
   })
 })
+
+function enterExpression(str) {
+  cy.get("input").type(str)
+}
 
 function clickButton(name) {
   cy.get("button")
     .contains(name)
     .click()
+}
+
+function waitForResult() {
+  cy.wait(500)
 }
