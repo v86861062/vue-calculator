900e93511276db0ca03b510da29ac40df8858760
v86861062 <v86861062@gmail.com>
2019-02-13 09:25:34 +0800

    Add e2e test: Basic function Test

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -6,3 +6,33 @@ describe("Smoke Test", () => {
     cy.title().should("eq", "vue-calculator")
   })
 })
+
+describe("Basic function Test", () => {
+  it("Use mouse to enter an expression", () => {
+    cy.visit("/")
+
+    clickButton("1")
+    clickButton("2")
+    clickButton("+")
+    clickButton("3")
+    cy.wait(500)
+    clickButton("=")
+    cy.contains("12+3 = 15")
+  })
+
+  it("Use keyboard to enter an expression", () => {
+    cy.visit("/")
+
+    cy.get("input").type("5^9")
+
+    cy.wait(500)
+    cy.get("input").type("{enter}")
+    cy.contains(`5^9 = ${Math.pow(5, 9)}`)
+  })
+})
+
+function clickButton(name) {
+  cy.get("button")
+    .contains(name)
+    .click()
+}
